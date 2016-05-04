dca42713dc271659a6d9e5a238335798b24e7ac9
Prashanth <pgopi@yahoo-inc.com>
2016-05-04 09:40:28 -0700

    Localizing the handlebar helper files.

--- load-hb-helpers.js
+++ load-hb-helpers.js
@@ -9,13 +9,14 @@ var path = require("path"),
 module.exports = function(Handlebars) {
   Handlebars = Handlebars || require("handlebars");
 
-  // Manually loading all the handlebar-helpers because 
+  // Manually loading all the handlebar-helpers because
   // it doesn't expose the Handlebars fn.
   // https://github.com/assemble/handlebars-helpers/issues/154
 
-  var helpers = path.join.bind(null, __dirname, 'node_modules', "handlebars-helpers", "lib", "helpers");
+  var helpers = path.join.bind(null, __dirname, "lib", "helpers");
 
-  var localHelpers = _.without(fs.readdirSync(helpers()), "helpers.js", 'helpers-markdown.js', "helpers-code.js");
+  var localHelpers = _.without(fs.readdirSync(helpers()), "helpers.js",
+    'helpers-markdown.js', "helpers-code.js");
 
   localHelpers = localHelpers.map(function(helper) {
     return path.join(helpers(), helper);
