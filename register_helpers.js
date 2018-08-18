2bd9316ba756a210ed4890d990216f37a0e92d26
Prashanth Iyer <munichlinux@gmail.com>
2018-08-18 13:11:00 -0700

    Updating helpers.

--- register_helpers.js
+++ register_helpers.js
@@ -2,9 +2,10 @@ const helpers = require('handlebars-helpers');
 const layouts = require('handlebars-layouts');
 
 module.exports = Handlebars => {
-  Handlebars = Handlebars.registerHelper(layouts(Handlebars));
   helpers({
     Handlebars,
   });
+
+  Handlebars = Handlebars.registerHelper(layouts(Handlebars));
   return Handlebars;
 };
